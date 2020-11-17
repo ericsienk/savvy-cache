241947746a73e663e58ffaf175537c5f1c3397f5
Eric Sienkiewicz <ericsienk@gmail.com>
2020-11-16 20:59:46 -0500

    impl

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,37 @@
-import { hi } from ".";
+import savvy from ".";
 
-describe("main export", () => {
-  it("should defined", () => {
-    expect(hi).toBeDefined();
+describe("savvy", () => {
+  it("should return cache before secondsUntilRefresh", async () => {
+    let returnedValue = 0;
+    const cache = savvy(() => returnedValue, 60);
+    let currentTime = Date.now();
+    const dateMock = jest
+      .spyOn(global.Date, "now")
+      .mockImplementation(() => currentTime);
+
+    expect(await cache.get()).toEqual(0);
+
+    returnedValue = 1;
+    currentTime += 59;
+    expect(await cache.get()).toEqual(0);
+
+    dateMock.mockRestore();
+  });
+
+  it("should break cache after secondsUntilRefresh", async () => {
+    let returnedValue = 0;
+    const cache = savvy(() => returnedValue, 60);
+    let currentTime = Date.now();
+    const dateMock = jest
+      .spyOn(global.Date, "now")
+      .mockImplementation(() => currentTime);
+
+    expect(await cache.get()).toEqual(0);
+
+    returnedValue = 1;
+    currentTime += 60 * 1000;
+    expect(await cache.get()).toEqual(1);
+
+    dateMock.mockRestore();
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-export const hi = "hi";
+export default <T>(
+  getValueToCache: () => Promise<T> | T,
+  secondsUntilRefresh: number
+) => {
+  let lastRefreshTimestamp = 0;
+  let lastCachedValue: T;
+  return {
+    get: async (): Promise<T> => {
+      if ((Date.now() - lastRefreshTimestamp) / 1000 >= secondsUntilRefresh) {
+        lastCachedValue = await getValueToCache();
+        lastRefreshTimestamp = Date.now();
+      }
+
+      return lastCachedValue;
+    },
+  };
+};
